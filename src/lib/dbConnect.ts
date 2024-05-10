0ccf6a1268eafd70be874e525ef8cfe566b63279
Yash <randomyashh@gmail.com>
2024-05-10 22:20:26 +0530

    Add 'lib' folder with database connection and utility files
    - Added 'lib' folder to the project root
    - Included 'dbConnect.tsx' file for database connection setup
    - Included 'utils.tsx' file for utility functions
    
    This commit lays the foundation for database connectivity and provides utility functions for future development tasks.

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+
+type ConnectionObject = {
+    isConnected?: number
+}
+
+const connection : ConnectionObject = {}
+
+export default async function databaseConnect(): Promise<void> {
+    if (connection.isConnected) { // checking the database connection to prevent the chocking//
+        console.log("Database is already established");
+        return
+    }
+
+    try {
+        const db = await mongoose.connect(process.env.MONGODB_URI || '', {})
+        //trying to connect to a database by providing database, either we're providing the empty string to fetch the error.
+        connection.isConnected = db.connections[0].readyState
+
+        console.log("Database Connection has been Established");
+
+        console.log(db);
+        console.log(db.connections);
+        
+    } catch (error) {
+        console.log("Database Connection has been lost", error);
+        
+        process.exit(1);
+    }
+}
