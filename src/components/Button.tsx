011441625750812530f21950e82e6efb421070ed
Yash <randomyashh@gmail.com>
2024-05-11 08:33:09 +0530

    modified:src/components/Button.tsx

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,31 +1,37 @@
 "use client";
-import React from "react";
+import React, { Children } from "react";
 import reactElementToJSXString from "react-element-to-jsx-string";
 import { toast, Toaster } from "sonner";
 import { ButtonsCard } from "@/components/ui/tailwindcss-buttons";
 
-function Button() {
+interface ButtonProps {
+  onClick: () => void;
+  children: React.ReactNode;
+}
+
+const Button: React.FC<ButtonProps> = ({ onClick, children }) => {
   return (
-    <div className="pb-40 px-4 w-full">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-full  max-w-7xl mx-auto gap-10">
-        {buttons.map((button, idx) => (
-          <ButtonsCard key={idx}>{button.component}</ButtonsCard>
-        ))}
-      </div>
+    <div className="flex items-center justify-center h-40">
+      {buttons.map((button, idx) => (
+        <ButtonsCard key={idx}>{button.component}</ButtonsCard>
+      ))}
+      <button onClick={onClick}>{children}</button>
     </div>
   );
-}
+};
 export const buttons = [
   {
     name: "Get Started",
     showDot: false,
     component: (
-      <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
-        <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
-        <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
-          Get Started
-        </span>
-      </button>
+      <div className="flex justify-cente md:justify-center items-center lg:justify-center">
+        <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[2px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+          <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+          <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-gray-300 px-3 py-1 text-sm font-medium text-black backdrop-blur-3xl">
+            Get Started
+          </span>
+        </button>
+      </div>
     ),
   },
 ];
